0bc96bb12d3c2f1a4a213e110467e69c30637901
Rajarajeswari <51874515+Angiaraji@users.noreply.github.com>
2021-01-02 23:13:04 +0100

    Create diode.vhd

---
+++ AMS_model/diode.vhd
@@ -0,0 +1,23 @@
+LIBRARY IEEE;
+USE IEEE.ELECTRICAL_SYSTEMS.ALL;
+use ieee.math_real.all;
+
+ENTITY diode_E IS
+  generic( idealityfactor:real:=1.1;
+           thermalvoltage:real:=25.0e-3;
+           saturationcurrent:real:=1.0e-9);
+  PORT (
+    TERMINAL anode, cathode : ELECTRICAL);
+
+END diode_E;
+
+-- Architecture to be added by the students
+architecture ideal of diode_E is
+
+quantity u_d across i_d through anode to cathode;
+
+begin
+
+i_d == saturationcurrent*(exp(((u_d)/(idealityfactor*thermalvoltage))-1.0));
+
+end ideal;
